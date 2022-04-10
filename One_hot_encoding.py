608f13480416df6881a59c78bce9d2da236f339b
Rakesh <94432433+Rakesh-c5@users.noreply.github.com>
2022-04-10 22:24:05 +0530

    Delete One_hot_encoding.py

--- One_hot_encoding.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import copy
-
-df_flights=pd.read_csv("cse.csv")
-cat_onehot=df_flights.copy()
-cat_onehot=pd.get_dummies(cat_onehot,columns=['m1'],prefix=['m1'])
-print(cat_onehot)
