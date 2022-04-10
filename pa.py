ad8574764ee9b4d30e22d88d435171b9b04e3808
Rakesh <94432433+Rakesh-c5@users.noreply.github.com>
2022-04-10 22:24:52 +0530

    Delete pa.py

--- pa.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import copy
-df_flights=pd.read_csv("flights.csv")
-print(df_flights.info())
-cat_df_flights=df_flights.select_dtypes(include=['object'])
-print(cat_df_flights['UniqueCarrier'])
-
-
